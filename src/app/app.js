ca58ae8484a2885f3e2bdfc8653944658449755c
Anton Yegorov <an_tosha33@mail.ru>
2019-06-25 21:30:56 +0300

    a

--- src/app/app.js
+++ src/app/app.js
@@ -1,30 +1,55 @@
 export default class App {
-    constructor() {
-        this.currentTool = 'pen';
-        this.toolSize = '32';
-    }
+  constructor() {
+    this.currentTool = 'pen';
+    this.toolSize = '32';
+    this.color = 'black';
+  }
 
-    hoverToMove() {
-        const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-        const lastPosition = {
-            x: 0,
-            y: 0,
-        }
-        const previousRect = {
-            x: 0,
-            y: 0,
-        }
-        canvas.addEventListener('mousemove', (e) => {
-                console.log(e.offsetX/this.toolSize);
-                ctx.clearRect(previousRect.x, previousRect.y,  this.toolSize,  this.toolSize);
-                ctx.fillStyle = 'rgba(156, 156, 156, 0.4)';
-                ctx.fillRect(e.offsetX-this.toolSize/2 ,e.offsetY-this.toolSize/2, this.toolSize, this.toolSize);
-                previousRect.x = e.offsetX-this.toolSize/2;
-                previousRect.y = e.offsetY-this.toolSize/2;
-            });
-        canvas.addEventListener('mouseleave', (e) => {
-            ctx.clearRect(previousRect.x, previousRect.y,  this.toolSize,  this.toolSize);
-        });
-    }
+  hoverToMove() {
+    const canv = document.getElementById('canvas-overlay');
+    const ctx = canv.getContext('2d');
+    const previousRect = {
+      x: 0,
+      y: 0,
+    };
+    canv.addEventListener('mousemove', (e) => {
+      console.log(e.offsetX % 8)
+      if (e.offsetX % 32 === 0 || e.offsetY % 32 === 0) {
+        // ctx.clearRect(previousRect.x, previousRect.y, this.toolSize, this.toolSize);
+        // ctx.fillStyle = 'rgba(156, 156, 156, 0.4)';
+        ctx.fillRect(e.offsetX,
+          e.offsetY,
+          this.toolSize, this.toolSize);
+        previousRect.x = e.offsetX;
+        previousRect.y = e.offsetY;
+      }
+    });
+    canv.addEventListener('mouseleave', () => {
+      ctx.clearRect(previousRect.x, previousRect.y, this.toolSize, this.toolSize);
+    });
+    // canv.addEventListener('mousedown', () => {
+    //   canv.setAttribute("style", "z-index:-1");
+    //   this.drawPen(canv, previousRect);
+    // });
+  }
+
+  drawPen(canv, previousRect) {
+    const mainLayer = document.getElementById('canvas-drawing');
+    const ctx = mainLayer.getContext('2d');
+    // mainLayer.addEventListener('mousedown', (e) => {
+    //   ctx.fillStyle = this.color;
+    //   ctx.fillRect(e.offsetX,
+    //     e.offsetY,
+    //     this.toolSize, this.toolSize);
+    //     mainLayer.addEventListener('mousemove', (e) => {
+    //       ctx.fillStyle = this.color;
+    //       ctx.fillRect(e.offsetX,
+    //         e.offsetY,
+    //         this.toolSize, this.toolSize);
+    //     });
+    // });
+    // mainLayer.addEventListener('mouseup', (e) => {
+    //   canv.setAttribute("style", "z-index:10");
+    // });
+  }
 }
